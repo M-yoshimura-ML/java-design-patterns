ed155256bdc78de9e57abd395aabfaedb5aef925
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2024-12-23 11:49:20 +0800

    Abstract Factory begin

---
+++ src/main/java/com/company/abstractfactory2/Client.java
@@ -0,0 +1,12 @@
+package com.company.abstractfactory2;
+
+import com.company.abstractfactory2.aws.AwsResourceFactory;
+import com.company.abstractfactory2.gcp.GoogleResourceFactory;
+
+public class Client {
+
+    public static void main(String[] args) {
+    	
+    }
+
+}
---
+++ src/main/java/com/company/abstractfactory2/ResourceFactory.java
@@ -0,0 +1,6 @@
+package com.company.abstractfactory2;
+
+//Abstract factory with methods defined for each object type.
+public interface ResourceFactory {
+
+}
