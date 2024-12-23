a74b8405f38c2d0a95215fa573374c040cab2021
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2024-12-23 08:46:51 +0800

    Implement prototype design pattern

--- src/main/java/com/company/prototype/Client.java
+++ src/main/java/com/company/prototype/Client.java
@@ -2,8 +2,14 @@ package com.company.prototype;
 
 public class Client {
 
-	public static void main(String[] args) {
-        
+	public static void main(String[] args) throws CloneNotSupportedException {
+        Swordsman swordsman = new Swordsman();
+		swordsman.move(new Point3D(-10,0,0), 20);
+		swordsman.attack();
+		System.out.println(swordsman);
+
+		Swordsman s2 = (Swordsman) swordsman.clone();
+		System.out.println("Cloned swordsman:" + s2);
 	}
 
 }
--- src/main/java/com/company/prototype/GameUnit.java
+++ src/main/java/com/company/prototype/GameUnit.java
@@ -4,14 +4,29 @@ package com.company.prototype;
 /**
  * This class represents an abstract prototype & defines the clone method
  */
-public abstract class GameUnit {
+public abstract class GameUnit implements Cloneable{
 	
 	private Point3D position;
 	
 	public GameUnit() {
 		position = Point3D.ZERO;
 	}
-	
+
+	@Override
+	public GameUnit clone() throws CloneNotSupportedException {
+		//Shallow copy
+		GameUnit unit = (GameUnit) super.clone();
+		unit.initialize();
+		return unit;
+	}
+
+	protected void initialize() {
+		this.position = Point3D.ZERO;
+		reset();
+	}
+
+	protected abstract void reset();
+
 	public GameUnit(float x, float y, float z) {
 		position = new Point3D(x, y, z);
 	}
