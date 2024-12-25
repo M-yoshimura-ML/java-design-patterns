ca45ee215a208344306e20669ae4966f97540a14
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2024-12-25 13:23:27 +0800

    Adapter structural design pattern begin

---
+++ src/main/java/com/company/structural/adapter/Customer.java
@@ -0,0 +1,13 @@
+package com.company.structural.adapter;
+
+/*
+* Target interface required by new client code
+* */
+public interface Customer {
+
+    String getName();
+
+    String getDesignation();
+
+    String getAddress();
+}
