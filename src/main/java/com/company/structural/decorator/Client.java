c9edc72d2f526de1bd653a6d854cba4c3a3f7fed
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2024-12-31 21:22:56 +0800

    Implement decorator

--- src/main/java/com/company/structural/decorator/Client.java
+++ src/main/java/com/company/structural/decorator/Client.java
@@ -4,5 +4,11 @@ public class Client {
     public static void main(String[] args) {
         Message msg = new TextMessage("The <FORCE> is strong with this one!");
         System.out.println(msg.getContent());
+
+        Message decorator = new HtmlEncodedMessage(msg);
+        System.out.println(decorator.getContent());
+
+        decorator = new Base64EncodedMessage(decorator);
+        System.out.println(decorator.getContent());
     }
 }
