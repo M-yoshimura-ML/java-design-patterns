f127f75f08953ac43d0b5c9715eabad0efb6cd06
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2025-01-06 14:37:46 +0800

    Implement Facade pattern

--- src/main/java/com/company/structural/facade/email/EmailFacade.java
+++ src/main/java/com/company/structural/facade/email/EmailFacade.java
@@ -1,4 +1,18 @@
 package com.company.structural.facade.email;
 
+import com.company.structural.facade.Order;
+
 public class EmailFacade {
+    public boolean sendOrderEmail(Order order) {
+        Template template = TemplateFactory.createTemplateFor(Template.TemplateType.Email);
+        Stationary stationary = StationaryFactory.createStationary();
+        Email email = Email.getBuilder()
+                .withTemplate(template)
+                .withStationary(stationary)
+                .forObject(order)
+                .build();
+
+        Mailer mailer = Mailer.getMailer();
+        return mailer.send(email);
+    }
 }
