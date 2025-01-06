a3b3f4777897e4174b1e0e6e902df753db5fd990
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2025-01-06 11:24:45 +0800

    Implement Composite design pattern

---
+++ src/main/java/com/company/structural/composite/File.java
@@ -0,0 +1,28 @@
+package com.company.structural.composite;
+
+/*
+* The component base class for composite design pattern
+* defines Operations applicable both leaf & composite
+* */
+public abstract class File {
+    private String name;
+    public File(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void ls();
+
+    public abstract void addFile(File file);
+
+    public abstract File[] getFiles();
+
+    public abstract boolean removeFile(File file);
+}
