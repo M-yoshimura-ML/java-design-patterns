aa76a9cf66ccbe20fb4d9d0619960d554986d057
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2025-02-03 14:56:24 +0800

    Implement mediator

---
+++ src/main/java/com/company/behavioral/mediator/UIMediator.java
@@ -0,0 +1,16 @@
+package com.company.behavioral.mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UIMediator {
+    List<UIControl> colleagues = new ArrayList<>();
+
+    public void register(UIControl control) {
+        colleagues.add(control);
+    }
+
+    public void valueChanged(UIControl control) {
+        colleagues.stream().filter(c->c!=control).forEach(c->c.controlChanged(control));
+    }
+}
